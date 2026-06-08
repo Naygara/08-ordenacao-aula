4bebfa41fc31515b9154b6b483009818388beb4e
Salazar <lsalazar@ciasc.sc.gov.br>
2026-06-08 18:49:25 -0300

    feat: insertion_sort inicial

---
+++ insertion_sort.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void imprimir(const vector<int>& v, const string& label = "") {
+    if (!label.empty()) cout << label << ": ";
+    cout << "[ ";
+    for (int x : v) cout << x << " ";
+    cout << "]\n";
+}
+
+void insertionSort(vector<int>& v) {
+    // todo: Implementar
+}
+
+int main() {
+    vector<int> aleatorio = {64, 25, 12, 22, 11};
+    imprimir(aleatorio, "Antes ");
+    insertionSort(aleatorio);
+    imprimir(aleatorio, "Depois");
+
+    return 0;
+}
